ddc59bb4a576fe64d6c22d9f928772e7f729e0ac
Ethan Santos <56823022+EASantos21@users.noreply.github.com>
2022-03-16 20:48:02 -0700

    Update history-template.js
    Updated comments

--- history-template.js
+++ history-template.js
@@ -10,10 +10,15 @@ var incorrectMessages = [
     "It seems the answer given was wrong, give it another shot!"
 ]
 
+/*
+    Void function which checks whether an answer is correct on the given question/fieldName.
+    The function sends an alert message based on the result notifying the user of how they did.
+*/
 function checkQuestionMC(fieldName) {
-    //Setting boolean variable 'correct' to false
+    // Setting boolean variable 'correct' to false
     var correct = false
-        //Dictionary to hold values for our answer key 
+    // Our answer key to use to check if user is correct.
+    // Key: fieldName Value: Answer
     var answers = {
         question1: "Egypt",
         question2: "Catholicism",
@@ -24,10 +29,11 @@ function checkQuestionMC(fieldName) {
         question7: "867",
         question8: "True"
     };
-    //Finding the field/question with JQuery and seeing what the user input was
+    
+    // Finding the field/question with JQuery and seeing what the user input was
     let value = $('input[name = ' + fieldName + ' ]:checked').val()
-        //Checking if fieldname param is valid as well as if the user input matches the field
-        //name's correct value. If there is a match, correct is set to true
+    // Checking if fieldname param is valid as well as if the user input matches the field
+    // name's correct value. If there is a match, correct is set to true
     for (let key in answers) {
         if (fieldName == key) {
             if (answers[fieldName] == value) {
@@ -35,6 +41,7 @@ function checkQuestionMC(fieldName) {
             }
         }
     }
+    
     //Checks if correct is set to true and prints appropriate message
     if (correct == true) {
         alert(correctMessages[Math.floor(Math.random() * correctMessages.length)])
